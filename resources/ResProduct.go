5ea736a3be8af0b3eaf70315027c54904d09eb3c
Alex Colussi <alexcolussi@outlook.com>
2020-08-24 16:58:36 -0300

    [fix] erro handler unnecessary

--- resources/ResProduct.go
+++ resources/ResProduct.go
@@ -14,7 +14,7 @@ type ResProduct struct {
 	CostumerEmail   string     `json:"costumer_email"`
 	CostumerCpfCnj  string     `json:"costumer_cpf_cnj"`
 	CostumerType    int        `json:"costumer_type"`
-	Status          int        `json:"status"`
+	Status          string     `json:"status"`
 	StatusReason    *string    `json:"status_reason"`
 	DateUpdt        *time.Time `json:"date_updt"`
 	DateIns         *time.Time `json:"date_ins"`
@@ -31,11 +31,18 @@ func (r *ResProduct) ResProductResource(mod models.ResProduct) {
 	if mod.CostumerType == 0 {
 		r.CostumerCpfCnpj = fmt.Sprintf("%011s", strconv.Itoa(int(cpfCnpj)))
 	} else if mod.CostumerType == 1 {
-		r.CostumerCpfCnpj = fmt.Sprintf("%015s", strconv.Itoa(int(cpfCnpj)))
+		r.CostumerCpfCnpj = fmt.Sprintf("%014s", strconv.Itoa(int(cpfCnpj)))
 
 	}
 	r.CostumerType = mod.CostumerType
-	r.Status = mod.Status
+	if mod.Status == 0 {
+		r.Status = "Pendente"
+	} else if mod.Status == 1 {
+		r.Status = "Aprovado"
+	} else if mod.Status == 2 {
+		r.Status = "Rejeitado"
+	}
+
 	r.StatusReason = mod.StatusReason
 	r.DateUpdt = mod.DateUpdt
 	r.DateIns = mod.DateIns
